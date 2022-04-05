f4bb3344a73d599e23b257a6f6f64942154113c6
mynamegitrepos <samuelpereiradesouza665@gmail.com>
2022-04-05 00:20:04 -0300

    Templates

---
+++ autenticacao/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def cadastro(request):
+  if request.method == 'GET':
+    return render(request,'cadastro.html')
+  elif request.method == 'POST':
+    username = request.POST.get('username')
+    senha = request.POST.get('password')
+    confirmar_senha = request.POST.get('confirm-password')
+    print(f"{username} | {senha} | {confirmar_senha}")
+    return HttpResponse('Recebido')  
+
+def logar(request):
+    return HttpResponse('Logar')
